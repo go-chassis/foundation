8c7c5cdca7d7f6447da99f109a5d500e6882a578
dongjiang <dongjiang1989@126.com>
2022-12-20 12:16:32 +0800

    [feat] add workpool common library (#15)
    * dongjiang, add case
    
    * dongjiang, change atomic v1.9.0 support golang 1.16.x
    
    * Create golangci-lint.yml
    
    update golangci/golangci-lint-action@v2 to golangci/golangci-lint-action@v3
    
    * Update golangci-lint.yml
    
    set golang version 1.16, same as unittest golang version
    
    * dongjiang, update code review

---
+++ workpool/work_test.go
@@ -0,0 +1,86 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package workpool
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/atomic"
+)
+
+func Test_NewWorker(t *testing.T) {
+	assert := assert.New(t)
+	ctx, cancel := context.WithCancel(context.Background())
+	w := NewWorker(
+		&workerPool{
+			name:                "test",
+			maxWorkers:          2,
+			tasks:               make(chan *Task, tasksCapacity),
+			readyWorkers:        make(chan *worker, readyWorkerQueueSize),
+			idleTimeout:         time.Second * 5,
+			onDispatcherStopped: make(chan struct{}),
+			stopped:             *atomic.NewBool(false),
+			workersAlive:        *atomic.NewInt32(0),
+			workersCreated:      *atomic.NewInt32(0),
+			workersKilled:       *atomic.NewInt32(0),
+			tasksConsumed:       *atomic.NewInt32(0),
+			ctx:                 ctx,
+			cancel:              cancel,
+		},
+	)
+	t.Run("Task Set, Setting task", func(t *testing.T) {
+		w.execute(&Task{
+			ID: "taskid1",
+			F: func() {
+				fmt.Println("dongjiang1")
+			},
+		})
+	})
+	t.Run("Task Set, Setting task2", func(t *testing.T) {
+		w.execute(&Task{
+			ID: "taskid2",
+			F: func() {
+				fmt.Println("dongjiang2")
+			},
+		})
+		assert.True(true)
+	})
+
+	t.Run("Task Set, Setting task3", func(t *testing.T) {
+		w.execute(&Task{
+			ID: "taskid3",
+			F: func() {
+				fmt.Println("dongjiang3")
+			},
+		})
+		assert.True(true)
+	})
+
+	t.Run("Task Set Stop", func(t *testing.T) {
+		w.stop(func(chan *Task) {
+			fmt.Println("finished")
+		})
+		assert.True(true)
+	})
+
+	assert.True(true)
+}
